20b32d3cc49dd0013212d1e732ae0c9125d57b08
Gustavo Alovisi <gustavo.alovisi@gmail.com>
2020-08-13 11:11:23 -0300

    falei um pouco sobre Identificação e constraints

--- poisson_sports_ranking.R
+++ poisson_sports_ranking.R
@@ -1,14 +1,17 @@
 ########################################Poisson Sports Ranking################################
 #This script fits a Poisson Ranking model to Brazilian soccer data. 
-#We define X_ij ~ poisson(lambda) where lambda = o_i - d_j = expected goals from i against j on a match
-#o_i is the ofensive power of team i. This is usually computed as average(goals from i)\average(goals from league)
+#We define X_ij ~ poisson(lambda) where lambda = e(o_i - d_j) = expected goals from i against j on a match
+#Note that lambda is always > 0.
+#o_i is the ofensive power of team i. This is usually computed as average(goals from i)\average(goals from league) in soccer analysis.
 #d_i is the defensive power of team j. Usually computed as average(goals against j)\average(goals against league)
+#So, we will be bounding our parameters to be >= 0 even if lambda is always positive. 
 
 #Due to nature of this problem, log-likelihood function uses information from d_j to estimate o_i 
 #And information from o_i to estimate d_j
 #A typical approach to this kind of optimization is Block Relaxation, where first we optimize o_i, then d_j.
 #We also consider L-BFGS-B/optim and NR with inequality constraint. 
 
+set.seed(182)
 
 ##Entering real data from Campeonato Brasileiro 2018
 #The i_j entry indicates goals from i against j 
@@ -54,7 +57,7 @@ epslon = 1e-15 #we set a convergence criteria
 
 i = 1  
 
-#####Cyclick Coordinate Descent: 
+##################Cyclic Coordinate Descent: 
 #block updating: we estimate oMLE1 using dMLE1 and initial guesses, and then dMLE2 using oMLE1. 
 
 poisson_block_opt <- function(epslon, X, o, d, O, D){ #block optimization of X log-lik function
@@ -80,15 +83,24 @@ results <- poisson_block_opt(epslon = 1e-15, X = X, o=o, d=d, O=O, D=D) #poisson
 O_est_BO <- results[[1]] ##ofensive estimate
 D_est_BO <- results[[2]] ##defensive estimate
 
-O_est_BO <- O_est_BO[,3]
+O_est_BO <- O_est_BO[,3] ##getting the last step of optimization
 D_est_BO <- D_est_BO[,3]
 
 names(O_est_BO) <- names(D_est_BO) <- c("AMM","ATM","ATP","BAH","BOT","CEA","CHA","COR","CRU","FLA",
                                         "FLU","GRE","INT","PAL","PAR","SAN","SPA","SPT","VAS","VIT")
 
+head(sort(O_est_BO)) #worst offensive teams 
+#      PAR       FLU       CEA       COR       SPT       AMM 
+#0.5692171 0.8374811 0.8946395 0.9487067 0.9487067 1.0953102 
 
-#Direct -loglikelihood minimization using optin and L-BFGS-B
-#We use L-BFGS-B to add lower>0 bounds on our parameters
+head(sort(D_est_BO)) #worst defensive teams
+#      SPT       VIT       FLU       CHA       PAR       BOT 
+#0.5787865 0.6041043 0.7123179 0.7413055 0.7413055 0.8019301
+
+
+
+#Direct -loglikelihood minimization using optim and L-BFGS-B
+#We use L-BFGS-B to add lower>=0 bounds on our parameters
 ##########################################################
 
 ### simple loglikelihood function
@@ -104,7 +116,7 @@ logL = function(t,n){
 
 
 n <- 20 #20 teams
-o <- numeric(n)  
+o <- numeric(n) #starting ofensive/def parameters vector
 d <- numeric(n)
 t = c(o, d) #reunites ofensive and defensive parameters in a single vector
 
@@ -113,8 +125,9 @@ t0 = c(rep(2,20), rep(1,20))
 
 control <- list(factr = 1e3) #setting L-BFGS-B convergence with smaller eps
 
+
 est_pars <- optim(par = t0, fn = logL, method = 'L-BFGS-B', 
-                  lower =  c(rep(0.1,40), upper = c(rep(5, 40))),
+                  lower =  c(rep(0,40), upper = c(rep(5, 40))),
                   control = control, n=20)$par  #optmization
 
 O_est_LBFGSB <- est_pars[1:20]
@@ -122,18 +135,27 @@ D_est_LBFGSB <- est_pars[21:40]
 names(O_est_LBFGSB) <- names(D_est_LBFGSB) <- c("AMM","ATM","ATP","BAH","BOT","CEA","CHA","COR","CRU","FLA",
                                                 "FLU","GRE","INT","PAL","PAR","SAN","SPA","SPT","VAS","VIT")
 
+head(sort(O_est_LBFGSB)) #worst offensive teams
+#PAR      FLU      CEA      COR      SPT      AMM 
+#1.306286 1.574550 1.631709 1.685776 1.685776 1.832379
+
+head(sort(D_est_LBFGSB)) #worst deffensive teams
+#SPT      VIT      FLU      CHA      PAR      BOT 
+#1.315856 1.341174 1.449387 1.478375 1.478375 1.538999 
+
+
 
 #Direct -loglikelihood minimization using Newton-Rapson 
-#There is no lambda*g(x) but we need to positive-bound our parameters
+#There is no lambda*g(x) (equality constraint) but we need to positive-bound our parameters
 ##########################################################
 
-#problem: we need to set inequality bounds on our parameters (>0)
-#lets update our log-likelihood function to greatly penalize less/equal to 0 parameters
+#problem: we need to set inequality bounds on our parameters (>=0)
+#lets update our log-likelihood function to greatly penalize less than 0 parameters
 mod_logL <- function(t,n){
   temp = 0
   for(i in 1:n){
     for(j in 1:n){
-      if((t[i] <= 0) | (t[n+j] <= 0)){ #if parameter is <= 0, returns a big positive value to minimization of neg. log.lik
+      if((t[i] <= 0) | (t[n+j] < 0)){ #if parameter is <= 0, returns a big positive value to minimization of neg. log.lik
         temp <- -1e10
         return(-temp)
       }
@@ -146,11 +168,23 @@ mod_logL <- function(t,n){
   -temp
 }
 
-est_pars_NR <- nlm(mod_logL, p = t0, n = 20)$estimate ##optimization
+est_pars_NR <- nlm(mod_logL, p = t0, n = 20)$estimate ##Newton-Rhapson optimization
 O_est_NR <- est_pars_NR[1:20]
 D_est_NR <- est_pars_NR[21:40]
 names(O_est_NR) <- names(D_est_NR) <- c("AMM","ATM","ATP","BAH","BOT","CEA","CHA","COR","CRU","FLA",
                                         "FLU","GRE","INT","PAL","PAR","SAN","SPA","SPT","VAS","VIT")
+
+head(sort(O_est_NR))
+#PAR       FLU       CEA       COR       SPT       BOT 
+#0.9327795 1.2010351 1.2581940 1.3122595 1.3122595 1.4588604 
+
+head(sort(D_est_NR))
+#SPT       VIT       FLU       CHA       PAR       BOT 
+#0.9423375 0.9676555 1.0758714 1.1048593 1.1048593 1.1654840 
+
+
+
+
 
 
 ###########################################
@@ -158,6 +192,7 @@ names(O_est_NR) <- names(D_est_NR) <- c("AMM","ATM","ATP","BAH","BOT","CEA","CHA
 library(tidyverse)
 
 ##First, lets compare each team's 'absolute' score, being ofensive + defensive score for each method
+#Their sum of scores represent the 'absolute' rank of each team
 OD_est3 <- matrix(0, 20, 3)
 OD_est3[,1] <- O_est_BO + D_est_BO
 OD_est3[,2] <- O_est_LBFGSB + D_est_LBFGSB
@@ -174,7 +209,9 @@ ggplot(OD_est3, aes(x = parvalue, color = Method, fill= Method)) + geom_density(
 #We can see the 3 methods produced different optimizations, where Block-Opt produced Ofensive+Defensive parameter
 #distribution with lowest median, and L-BFGS-B produced O+D param. distr. with highest median. 
 #However, the three distributions seem to have a similar shape.
-
+#This is probably related to Identification issues on the optimization. 
+#To fix this we can add a equality constraint where sum(o_i) - sum(d_i) = 0. 
+#However, the ranking stays the same even though the 'level' difference of parameters: 
 
 ###Lets now check for the 5 better teams in each method:
 
@@ -207,11 +244,12 @@ exp(O_est_NR["ATP"] - D_est_NR["SPT"]) #lambda
 
 #Here, we can see the expected goals are the same in every method, even though the different parameter values. 
 
-#This suggests the three algorithms can be used to estimate this sports rank example. 
+#This suggests the three algorithms can be used to estimate this sports rank example, as long as we address Identification issue. 
 #However, L-BFGS-B and Block Optimization can be thought of more natural approaches, since the derivation of 
 #log-likelihood function is straightforward. 
 #In Newton-Rapson log-lik function, we had to add a conditional statement to manually bound the parameters. 
 #In this case, we could see adding this condition wouldn't give us bad funtion values, but in more complicated functions
 #this could cause problems. Also, this makes analytical gradient/hessian calculation less intuitive. 
-
+#If we were doing unbounded optimization on parameters (not bounding on >=0), we could just use Nelder-Mead on optim and also we wouldn't need to change likelihood function for Newton-Rhapson method.
+#Then, all three methods could be a good option. 
 
