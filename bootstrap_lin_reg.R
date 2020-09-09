3b0db52fc2a59fe3935b7852b81908633dd99a12
Gustavo Alovisi <gustavo.alovisi@gmail.com>
2020-09-09 11:17:26 -0300

    final v. testando com library de bootstrap

--- bootstrap_lin_reg.R
+++ bootstrap_lin_reg.R
@@ -1,18 +1,21 @@
 
 ############linear regression bootstrap
-library(tidyverse)
+library(dplyr)
 
 
 Y <- c(6.9, 5.1, 7.5, 11.1, 10.9, 4.2, 10.5, 6.8, 12.3, 14.3)
 X <- c(6.2, 5.1, 7.6, 2.5, 3.5, 9.4, 4.1, 6.3, 3.0, 0.8)
-data <- as_tibble(cbind(Y,X))
+data <- dplyr::as_tibble(cbind(Y,X))
 
-#a) construa um IC para b0 e b1 com 95% onde Y = b0 + b1*X + e
 
-B <- 10000 #10000 bootstraps 
 
-beta0b <- numeric(10000)
-beta1b <- numeric(10000)
+
+####a) construa um IC para b0 e b1 com 95% onde Y = b0 + b1*X + e
+
+B <- 5000 #5000 bootstraps 
+
+beta0b <- numeric(5000)
+beta1b <- numeric(5000)
 
 for (i in 1:B){
   sample <- dplyr::sample_n(data, 10, replace = T)
@@ -24,34 +27,59 @@ for (i in 1:B){
 hist(beta0b)
 hist(beta1b)
 
-quantile(beta0b, c(0.025, 0.975))
-quantile(beta1b, c(0.025, 0.975))
+quantile(beta0b, c(0.025, 0.975)) #IC para beta0
+#   2.5%    97.5% 
+#  12.52248 16.20401 
+
+quantile(beta1b, c(0.025, 0.975)) #IC para beta1
+#       2.5%      97.5% 
+#  -1.6825384 -0.8514722
+
+
+####podemos testar nosso bootstrap em relação a pacotes de bootstrap:
+library(car)
+model <- lm(Y ~ X)
+bootm <- car::Boot(model, R=5000)
+car::Confint(bootm, level=0.95, type="perc")
+
+#              Estimate   2.5 %    97.5 %
+#  (Intercept) 14.551831 12.53016 16.201080
+#X             -1.152955 -1.66820 -0.863465
+
+##de fato, nosso bootstrap parece ter dado bons resultados. 
 
 
 
-#b) Assumindo X dado e erro ~ N(0, sigma²) utilize bootstrap parametrico p/ obter as mesmas estimativas
+#####b) Assumindo X dado e erro ~ N(0, sigma²) utilize bootstrap parametrico p/ obter as mesmas estimativas
 
 #Queremos estimar sigma do erro ~ N(0, sigma²) 
 
 model <- lm(Y ~ X)
 coefs <- model$coefficients #betas iniciais estimados
-sigma <- sigma(model) #estimando sigma dos residuos (erro)
+sigma <- stats::sigma(model) #estimando sigma dos residuos (erro)
 
 coefs_boot <- c(0,0)
 
-for (i in 1:B){
-  y_n <- coefs[1] + coefs[2]*X + rnorm(10, mean = 0, sd = sigma) #geramos Y_boot com X de nossos dados, betas estimados e N(0, sigma^)
-  #sample <- dplyr::sample_n(data, 10, replace = T)
-  coefs_boot <- coef(lm(y_n ~ X)) #com Y_boot, estimamos beta_boot 
-  beta0b[i] <- coefs_boot[1]
+for (i in 1:B){ #vamos agora fazer o bootstrap parametrico
+  y_n <- coefs[1] + coefs[2]*X + rnorm(10, mean = 0, sd = sigma) #geramos Y_boot com X de nossos dados originais, betas estimados e N(0, sigma^)
+  coefs_boot <- coef(lm(y_n ~ X)) #com Y_boot, estimamos os beta_boot 
+  beta0b[i] <- coefs_boot[1] #salvamos os valores estimados para criarmos suas distribuições
   beta1b[i] <- coefs_boot[2]
 }
 
-hist(beta0b)
+hist(beta0b) #dist de beta0boot e beta1boot 
 hist(beta1b)
 
-quantile(beta0b, c(0.025, 0.975))
-quantile(beta1b, c(0.025, 0.975))
+quantile(beta0b, c(0.025, 0.975)) #IC para beta0 boot parametrico
+#     2.5%    97.5% 
+# 12.41991 16.60076 
+
+quantile(beta1b, c(0.025, 0.975)) #IC para beta1 boot parametrico
+#        2.5%      97.5%  
+#  -1.5495476 -0.7627046
+
+##podemos notar que o bootstrap parametrico produziu estimativas próximas ao bootstrap não parametrico. 
+
 
 
 ###c) Utilize um teste de permutação para verificar H0: B1 = 0 
